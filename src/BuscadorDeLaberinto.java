0533382d8490b7bab7a15fc5b492fea9b1740680
Ilyavosky <ilyacortesruiz@gmail.com>
2025-09-20 13:45:47 -0600

    Adición del método recursivo

--- src/BuscadorDeLaberinto.java
+++ src/BuscadorDeLaberinto.java
@@ -2,33 +2,50 @@ public class BuscadorDeLaberinto<T> {
     private T[][] laberinto;
     private T tesoro;
     private T pared;
+    private T camino;
     private T caminoVisitado;
+
 
     public BuscadorDeLaberinto() {
     }
 
-    public BuscadorDeLaberinto(T[][] laberinto, T tesoro, T pared, T caminoVisitado) {
+    public BuscadorDeLaberinto(T[][] laberinto, T tesoro, T pared, T camino ,T caminoVisitado) {
         this.laberinto = laberinto;
         this.tesoro = tesoro;
         this.pared = pared;
+        this.camino = camino;
         this.caminoVisitado = caminoVisitado;
+
     }
 
     public boolean resolver(int filaInicio, int colInicio){
-
+        return buscarCamino(filaInicio, colInicio);
     }
 
     private boolean buscarCamino(int fila, int col){
-        if (fila < 0 || fila >= laberinto.length) {
+        if (fila < 0 || fila >= laberinto.length || col <0 || col >= laberinto[0].length) {
             return false;
-        } else if (laberinto[fila][col].equals(pared)) {
-            return false;
-        } else if (laberinto[fila][col].equals(caminoVisitado)) {
+        } else if (laberinto[fila][col].equals(pared) || laberinto[fila][col].equals(caminoVisitado)) {
             return false;
         } else if (laberinto[fila][col].equals(tesoro)) {
             return true;
-        } else if () {
-
         }
+        laberinto[fila][col] = caminoVisitado;
+
+        boolean encontrado = buscarCamino(fila + 1, col);
+
+        if (!encontrado){
+            encontrado = buscarCamino(fila, col + 1);
+        }
+        if (!encontrado) {
+            encontrado = buscarCamino(fila - 1, col);
+        }
+        if (!encontrado) {
+            encontrado = buscarCamino(fila, col - 1);
+        }
+        if (!encontrado) {
+            laberinto[fila][col] = camino;
+        }
+        return encontrado;
     }
 }
